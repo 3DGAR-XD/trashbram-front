7ea67861f5536c953a45da7a0fa2d887216ef8ac
3DGAR-XD <ejlc2001@gmail.com>
2022-05-07 06:55:01 -0500

    dev

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <h1>TrashBram, With friends is better 😉</h1>
   <nav>
   <ul>
+  <router-link to="/">Home</router-link>
   <router-link to="/login">Sign In</router-link>
   <router-link to="signup">Sign Up </router-link>
   </ul>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const { createApp } = require('vue');
 import Home from "./App.vue";
-import router from "./router/router.js"
+import router from "./router"
 
 createApp(Home).use(router).mount("#app");
