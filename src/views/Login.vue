463f97bd76116cd8985456b7df6b9abc86ce824f
3DGAR XD <85646467+3DGAR-XD@users.noreply.github.com>
2022-05-07 17:16:37 -0500

    dev

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,19 +1,37 @@
 <template>
-<form method="post" prevent="login">
-  <div class="form-floating mb-3">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-        <label for="floatingInput">Email address</label>
-        </div>
-        <div class="form-floating">
-          <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword">Password</label>
-            </div>
-  
+  <form method="post" @submit.prevent="login">
+    <div class="form-floating mb-3">
+      <input
+        type="email"
+        class="form-control"
+        id="floatingInput"
+        placeholder="name@example.com"
+      />
+      <label for="floatingInput">Email</label>
+    </div>
+    <div class="form-floating">
+      <input
+        type="password"
+        class="form-control"
+        id="floatingPassword"
+        placeholder="Password"
+        pattern="[A-Zaa-z]{10}"
+      />
+      <label for="floatingPassword">Password</label>
+    </div>
+    <button type="submit" class="btn btn-outline-secondary">
+      Iniciar Sesion
+    </button>
   </form>
 </template>
 
 <script>
 export default {
   name: "login",
+  methods: {
+    login() {
+      console.log("logueado")
+    }
+  }
 };
 </script>
